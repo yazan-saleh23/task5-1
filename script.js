9628562c35dcf987e92e39db9937e1ede904c1e8
yazan-saleh23 <yazan.f.saleh@gmail.com>
2023-10-11 10:51:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let output = document.getElementById("calc")
+output.value= '';
+function display(num) {
+    output.value += num;
+}
+function Clear() {
+    output.value= '';
+}
+function Delete() {
+    output.value = output.value.slice(0, -1);
+}
+function calc() {
+    try {
+        output.value = eval(output.value);
+    }
+    catch(err) {
+        output.value = "Error";
+    }
+    
+}
